a8ea71724161fdb59c961650a9a0ffe207575a18
stabri <paulstarbi@gmail.com>
2019-04-18 14:08:08 +0200

    basic header and carousel

---
+++ src/app/tracermap/tracermap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracermap',
+  templateUrl: './tracermap.component.html',
+  styleUrls: ['./tracermap.component.css']
+})
+export class TracermapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
